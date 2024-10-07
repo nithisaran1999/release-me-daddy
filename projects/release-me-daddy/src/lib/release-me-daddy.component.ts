1d411461277a2ac1fc75f9a948ba98f02ca1672e
Nithisaran  Suksom <nithisaran.suksom@gmail.com>
2024-10-07 07:06:16 +0000

    generate lib and show demo

---
+++ projects/release-me-daddy/src/lib/release-me-daddy.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-release-me-daddy',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      release-me-daddy works!
+    </p>
+  `,
+  styles: ``
+})
+export class ReleaseMeDaddyComponent {
+
+}
